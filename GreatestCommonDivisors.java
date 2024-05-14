357a6c9f54f2dad6b160b5a8c877e080b342e96e
vishalgaur752 <vishalgaur752@gmail.com>
2024-05-14 15:11:05 +0530

    Add files

---
+++ GreatestCommonDivisors.java
@@ -0,0 +1,5 @@
+public class GreatestCommonDivisors {
+    public static void main(String[] args) {
+        
+    }
+}
--- PalindromNum.java
+++ PalindromNum.java
@@ -1,5 +1,21 @@
 public class PalindromNum {
+    public static boolean isPalindrome(int num) {
+        int temp = num;
+        int rev = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            rev = (rev * 10) + digit;
+            num = num / 10;
+        }
+        if (rev == temp) {
+            return true;
+        }
+        return false;
+
+    }
+
     public static void main(String[] args) {
-        
+        int num = 121211;
+        System.out.println(isPalindrome(num));
     }
 }
