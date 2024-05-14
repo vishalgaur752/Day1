4ce4077b0b1c12f2c96b49332ba9642bed2287dd
vishalgaur752 <vishalgaur752@gmail.com>
2024-05-14 15:34:56 +0530

    Modify this file

--- GreatestCommonDivisors.java
+++ GreatestCommonDivisors.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class GreatestCommonDivisors {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        sc.close();
+        while (n1 != n2) {
+            if (n1 > n2) {
+                n1 = n1 - n2;
+            } else {
+                n2 = n2 - n1;
+            }
+        }
+        System.out.println("the greatest common divisor is : " + n1);// we can use n2.
     }
 }
